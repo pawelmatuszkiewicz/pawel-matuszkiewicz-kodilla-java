121d0e0cf99cc0a3d638da0ea4268303d2f0f64c
Pawel Matuszkiewicz <pmatuszk@gmail.com>
2018-02-13 15:10:51 +0100

    Restore Calculator unit test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,6 +1,27 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+        Calculator calculator = new Calculator();
+
+        int cresult = calculator.add(22, 77);
+        if (cresult == 99) {
+            System.out.println("Calculator.add() test OK.");
+        }
+        else {
+            System.out.println("Calculator.add() Error!");
+        }
+
+        cresult = calculator.subtract(123, 100);
+        if (cresult == 23) {
+            System.out.println("Calculator.subtract() test OK.");
+        }
+        else {
+            System.out.println("Calculator.subtract() Error!");
+        }
     }
 }
