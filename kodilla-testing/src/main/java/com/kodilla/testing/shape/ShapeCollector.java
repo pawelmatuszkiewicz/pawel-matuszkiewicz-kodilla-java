61028d3d4b0b1aae5102f8a7427715041ec13116
Pawel Matuszkiewicz <pmatuszk@gmail.com>
2018-02-13 20:46:50 +0100

    Implement ShapeCollector

--- kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -6,22 +6,33 @@ public class ShapeCollector {
     private ArrayList<Shape> shapes = new ArrayList<Shape>();
 
     public void addFigure(Shape shape) {
-
+        shapes.add(shape);
     }
 
     public boolean removeFigure(Shape shape) {
-        return true;
+        return shapes.remove(shape);
     }
 
     public Shape getFigure(int n) {
-        return null;
+        Shape shape = null;
+
+        if(n >= 0 && n < shapes.size()) {
+            shape = shapes.get(n);
+        }
+
+        return shape;
     }
 
     public void showFigures() {
 
+        int i = 1;
+        for(Shape shape : shapes) {
+            System.out.println("Figure #" + i++ + "  " + shape.getShapeName()
+                                + "  area: " + shape.getField());
+        }
     }
 
     public int getFiguresQuantity() {
-        return 111;
+        return shapes.size();
     }
 }
