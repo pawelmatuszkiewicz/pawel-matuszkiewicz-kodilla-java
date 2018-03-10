94bfd8aa434b10119cb22a012332805d09f1b7f5
Pawel Matuszkiewicz <p.matuszkiewicz@accrea.com>
2018-03-10 18:38:59 +0100

    Add training project for Spring module of bootcamp

---
+++ kodilla-spring-intro/src/test/java/DrawerTestSuite.java
@@ -0,0 +1,29 @@
+import com.kodilla.spring.intro.Circle;
+import com.kodilla.spring.intro.Drawer;
+import com.kodilla.spring.intro.Triangle;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DrawerTestSuite {
+    @Test
+    public void testDoDrawingWithCircle() {
+        //Given
+        Circle circle = new Circle();
+        //When
+        Drawer drawer = new Drawer(circle);
+        String result = drawer.doDrawing();
+        //Then
+        Assert.assertEquals("This is a circle", result);
+    }
+
+    @Test
+    public void testDoDrawingWithTriangle() {
+        //Given
+        Triangle triangle = new Triangle();
+        //When
+        Drawer drawer = new Drawer(triangle);
+        String result = drawer.doDrawing();
+        //Then
+        Assert.assertEquals("This is a triangle", result);
+    }
+}
